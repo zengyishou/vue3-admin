fd4fa7d80ec54a44bd7303de80669ed2ff97ddb3
曾怡寿 <zengyishou@foxmail.com>
2022-08-05 18:46:02 +0800

    chore(custom): 添加normalize.css及scss支持

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+// 通过引入normalize.css让不同的浏览器在渲染网页元素的时候形式更统一
+import 'normalize.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+// 从 @element-plus/icons-vue 中导入所有图标并进行全局注册
+for (const [key, component] of Object.entries(ElementPlusIconsVue))
+  app.component(key, component)
 
 app.mount('#app')
