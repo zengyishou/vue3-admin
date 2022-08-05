2e9a3f6cfb1c24eb3311fce1daa8b7a4c1a1f6e4
曾怡寿 <zengyishou@foxmail.com>
2022-08-05 17:26:19 +0800

    chore(custom): 引入Element Plus组件库并配置自动导入

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,20 @@ import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
