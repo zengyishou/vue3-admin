0f71298963cec4a1e50dd2cd1929b79aad515776
曾怡寿 <zengyishou@foxmail.com>
2022-08-05 16:43:20 +0800

    使用@antfu/eslint-config约束代码格式规范

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,14 +2,16 @@
 defineProps({
   msg: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
 })
 </script>
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="green">
+      {{ msg }}
+    </h1>
     <h3>
       You’ve successfully created a project with
       <a target="_blank" href="https://vitejs.dev/">Vite</a> +
