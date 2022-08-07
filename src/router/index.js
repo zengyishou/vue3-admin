4656071682de1e649fef502f657305745fb019d5
曾怡寿 <zengyishou@foxmail.com>
2022-08-07 14:03:02 +0800

    feat(custom): 使用全局前置守卫完成登录鉴权

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useUserStore } from '@/stores/user'
 
 const publicRoutes = [
   {
@@ -16,6 +17,25 @@ const publicRoutes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: publicRoutes,
+})
+
+// 路由白名单
+const whiteList = ['/login']
+
+// 路由前置守卫
+router.beforeEach(async (to, from, next) => {
+  if (useUserStore().getToken) {
+    if (to.path === '/login')
+      next('/')
+    else
+      next()
+  }
+  else {
+    if (whiteList.includes(to.path))
+      next()
+    else
+      next('/login')
+  }
 })
 
 export default router
--- src/stores/user.js
+++ src/stores/user.js
@@ -1,14 +1,26 @@
 import { defineStore } from 'pinia'
 import { login } from '@/api/sys'
+import { getItem, setItem } from '@/utils/storage'
+import { TOKEN } from '@/constant/index'
 
 export const useUserStore = defineStore('user', {
-  state: () => { return {} },
-  getters: {},
+  state: () => {
+    return {
+      token: getItem(TOKEN) || '',
+    }
+  },
+  getters: {
+    getToken: (state) => {
+      return state.token
+    },
+  },
   actions: {
-    login: (username, password) => {
+    login(username, password) {
       return new Promise((resolve, reject) => {
         login({ username, password })
           .then((data) => {
+            this.token = data.token
+            setItem(TOKEN, data.token)
             resolve(data)
           })
           .catch((error) => {
